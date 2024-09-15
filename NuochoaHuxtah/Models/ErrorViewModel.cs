8870682adac1b6df1383332f4dfce424a8bb4c53
PC <PC@DESKTOP-85J58PV>
2024-09-15 10:59:51 +0700

    Add project files.

---
+++ NuochoaHuxtah/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NuochoaHuxtah.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
