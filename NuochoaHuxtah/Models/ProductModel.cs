d8e9236db55382575f8921b6fbfa12123ae3b717
HuuThanh <nhtthanh633@gmail.com>
2024-09-22 14:30:44 +0700

    Tạo trang admin, hiển thị sản phẩm, thêm mới sản phẩm

--- NuochoaHuxtah/Models/ProductModel.cs
+++ NuochoaHuxtah/Models/ProductModel.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace NuochoaHuxtah.Models
 {
@@ -7,26 +9,32 @@ namespace NuochoaHuxtah.Models
         [Key]
         public int Id { get; set; }
 
-		[Required, MinLength(4, ErrorMessage = "Yêu cầu nhập tên sản phẩm")]
+		[Required( ErrorMessage = "Yêu cầu nhập tên sản phẩm")]
 		public string Name { get; set; }
 
 		public string Slug { get; set; }
 
-		[Required, MinLength(4, ErrorMessage = "Yêu cầu nhập mô tả sản phẩm")]
+		[Required( ErrorMessage = "Yêu cầu nhập mô tả sản phẩm")]
 		public string Description { get; set; }
 
-		[Required, MinLength(4, ErrorMessage = "Yêu cầu nhập giá sản phẩm")]
+		[Required( ErrorMessage = "Yêu cầu nhập giá sản phẩm")]
+        [Range(0.01, double.MaxValue)]
+        [Column(TypeName = "decimal(8,2)")]
 		public decimal Price { get; set; }
-
-        public int BrandId { get; set; }
-
-        public int CategoryId { get; set; }
+		[Required, Range(1,int.MaxValue, ErrorMessage = "Chọn một thương hiệu")]
+		public int BrandId { get; set; }
+		[Required, Range(1, int.MaxValue, ErrorMessage = "Chọn một danh mục")]
+		public int CategoryId { get; set; }
 
         public CategoryModel Category { get; set; }
 
         public BrandModel Brand { get; set; }
 
-        public string Image { get; set; }
+        public string Image { get; set; } = "noimage.jpg";  
+
+        [NotMapped]
+        [FileExtensions]
+        public IFormFile ImageUpload { get; set; }
 
     }
 }
---
+++ NuochoaHuxtah/Repository/Validation/FileExtensionAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NuochoaHuxtah.Repository.Validation
+{
+    public class FileExtensionAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is IFormFile file)
+            {
+                var extention = Path.GetExtension(file.FileName); //123.jpg
+                string[] extensions = {"jpg", "png","jpeg" };   
+
+                bool result = extensions.Any(x => extention.EndsWith(x));
+                if(!result)
+                {
+                    return new ValidationResult("Allowed extension are jpg or png or jpeg");
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
